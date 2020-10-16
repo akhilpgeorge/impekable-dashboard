716c86f88ee380cb4170276084f2df7a3c4e2f71
akhilpgeorge <akhilpgeorge@gmail.com>
2020-10-16 13:20:34 +0530

    Updates

---
+++ precache-manifest.9075cca07800c4a1cc405057ff59ef9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ebb9cea9301868f34818a943e0177c",
+    "url": "/impekable-dashboard/index.html"
+  },
+  {
+    "revision": "0b69ab254c31c25d89aa",
+    "url": "/impekable-dashboard/static/css/main.90a00745.chunk.css"
+  },
+  {
+    "revision": "9d7c67e8f552d56e5af4",
+    "url": "/impekable-dashboard/static/js/2.56bbdfc0.chunk.js"
+  },
+  {
+    "revision": "df0e86ee792537fa087848a56c97c692",
+    "url": "/impekable-dashboard/static/js/2.56bbdfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b69ab254c31c25d89aa",
+    "url": "/impekable-dashboard/static/js/main.cead1b6c.chunk.js"
+  },
+  {
+    "revision": "23a921c6050199f81031",
+    "url": "/impekable-dashboard/static/js/runtime-main.a3d9676f.js"
+  },
+  {
+    "revision": "3dbb9646806ddb7c15df28dfa02ce9da",
+    "url": "/impekable-dashboard/static/media/JanaNavakova.3dbb9646.jpg"
+  }
+]);
